62e557d47ca8278301c0f18dec5c9c053acdbb2e
DiegoSullon <67489081+DiegoSullon@users.noreply.github.com>
2021-05-31 14:47:29 -0500

    commentariesByUser

--- app/src/app/pages/news/article/article.component.ts
+++ app/src/app/pages/news/article/article.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Commentary } from 'src/app/models/commentary';
 import { Publication } from 'src/app/models/publication';
+import { CommmentaryService } from 'src/app/services/commentary.service';
 
 @Component({
   selector: 'bh-article',
@@ -10,11 +12,20 @@ export class ArticleComponent implements OnInit {
   @Input()
   publication: Publication;
 
+  commentaries: Commentary[]=[];
+
   format: string = 'dd/MM/yyyy';
-  constructor() { }
+  constructor(private commentaryService: CommmentaryService) { }
 
   ngOnInit(): void {
-
+    this.fetchComentaries();
+  }
+  fetchComentaries() {
+    this.commentaryService.getComentariesByPublication(this.publication.idPublicacion).subscribe(data=>{
+      this.commentaries=data;
+    },err=>{
+      console.log("Error: ",err);
+    })
   }
 
 }
