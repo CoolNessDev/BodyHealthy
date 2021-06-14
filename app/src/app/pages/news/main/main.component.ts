394893fb05fbd06f86a32bf37818ba70c1a071ff
DiegoSullon <67489081+DiegoSullon@users.noreply.github.com>
2021-06-14 13:03:23 -0500

    fromgroup publication

--- app/src/app/pages/news/main/main.component.ts
+++ app/src/app/pages/news/main/main.component.ts
@@ -1,29 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Publication } from 'src/app/models/publication';
 import { PublicationService } from 'src/app/services/publication.service';
 
 @Component({
   selector: 'bh-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
-  publications: Publication[]=[];
+  publicationForm: FormGroup;
+  publications: Publication[] = [];
+  // img
+  imagen: File;
+  imagenSrc: File;
+  newImg: boolean = false;
   // pageable
   totalPublications: number = 10;
   totalPages: number = 4;
   pageSize: number = 4;
   currentPage: number = 1;
-  constructor(private publicationService: PublicationService) { }
+  constructor(private publicationService: PublicationService) {}
 
   ngOnInit(): void {
     this.fetchPublications();
+    this.publicationForm = new FormGroup({
+      message: new FormControl(null, Validators.required),
+    });
   }
   fetchPublications() {
-    this.publicationService.getPublicationsByPages(this.currentPage-1,this.pageSize,"fecha",true).subscribe(data=>{
-      this.publications = data.content;
-    },err=>{
-      console.log("Error: ",err);
-    })
+    this.publicationService
+      .getPublicationsByPages(
+        this.currentPage - 1,
+        this.pageSize,
+        'fecha',
+        true
+      )
+      .subscribe(
+        (data) => {
+          this.publications = data.content;
+        },
+        (err) => {
+          console.log('Error: ', err);
+        }
+      );
   }
+  onFileChange(event) {
+    this.imagen = event.target.files[0];
+    const fr = new FileReader();
+    fr.onload = (evento: any) => {
+      this.newImg=true;
+      this.imagenSrc = evento.target.result;
+    };
+    fr.readAsDataURL(this.imagen);
+  }
+  reset(): void {
+    this.imagen = null;
+    this.imagenSrc = null;
+  }
+  onPublicate = () => {};
 }
