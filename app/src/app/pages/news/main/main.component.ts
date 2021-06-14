8543fefcbf35ac0eaa7590f7189745dabd88a841
DiegoSullon <67489081+DiegoSullon@users.noreply.github.com>
2021-06-14 13:12:43 -0500

    user name and img in main

--- app/src/app/pages/news/main/main.component.ts
+++ app/src/app/pages/news/main/main.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Publication } from 'src/app/models/publication';
+import { User } from 'src/app/models/user';
 import { PublicationService } from 'src/app/services/publication.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'bh-main',
@@ -9,6 +11,10 @@ import { PublicationService } from 'src/app/services/publication.service';
   styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
+  // Main user
+  userNames: string;
+  userImg: string;
+  // publication
   publicationForm: FormGroup;
   publications: Publication[] = [];
   // img
@@ -20,13 +26,18 @@ export class MainComponent implements OnInit {
   totalPages: number = 4;
   pageSize: number = 4;
   currentPage: number = 1;
-  constructor(private publicationService: PublicationService) {}
+  constructor(
+    private publicationService: PublicationService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.fetchPublications();
     this.publicationForm = new FormGroup({
       message: new FormControl(null, Validators.required),
     });
+    this.userNames = this.userService.getNames();
+    this.userImg = this.userService.getUserImg();
   }
   fetchPublications() {
     this.publicationService
@@ -49,7 +60,7 @@ export class MainComponent implements OnInit {
     this.imagen = event.target.files[0];
     const fr = new FileReader();
     fr.onload = (evento: any) => {
-      this.newImg=true;
+      this.newImg = true;
       this.imagenSrc = evento.target.result;
     };
     fr.readAsDataURL(this.imagen);
