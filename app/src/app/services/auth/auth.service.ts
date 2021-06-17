2ce5f31ad978c6521de942ebb11e3bd2f7035117
DiegoSullon <67489081+DiegoSullon@users.noreply.github.com>
2021-06-17 18:43:48 -0500

    registerUser

--- app/src/app/services/auth/auth.service.ts
+++ app/src/app/services/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { JwtDTO } from 'src/app/models/jwtDto';
 import { LoginUserDto } from 'src/app/models/loginUserDto';
+import { User } from 'src/app/models/user';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +13,8 @@ export class AuthService {
   constructor(private httpClient: HttpClient) {}
   public login(loginUsuario: LoginUserDto): Observable<JwtDTO> {
     return this.httpClient.post<JwtDTO>(this.authURL + 'login', loginUsuario);
+  }
+  public createUser(user: User): Observable<any> {
+    return this.httpClient.post<any>(`${this.authURL}create`, user);
   }
 }
